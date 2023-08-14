df0b64c72886d547cc2209d95864362c8940d10e
vaisakhDev <44577910+vaisakhDev@users.noreply.github.com>
2023-08-15 00:19:40 +0530

    reactor: use react-three fiber components, add random rotation for each branch, use leva for changing state :sparkles:

--- src/App.js
+++ src/App.js
@@ -1,105 +1,25 @@
 import "./App.css";
 
 import CanvasContainer from "./CanvasContainer";
-import Layout, { Content } from "antd/es/layout/layout";
-import Sider from "antd/es/layout/Sider";
-import { getCollatzSequence } from "./helpers";
 import * as THREE from "three";
 import React, { useState } from 'react';
 import { useControls } from 'leva'
 
-
-
-
-
-const d = 10;
+function App() {
+    const d = 10;
 const alpha = Math.PI / 13;
 const vectorsList = [];
+const n = 100;
 
-function createSplinePathFromCollatz(sequence) {
-    const points = [];
-    let prevPoint;
-    let relativeAngle = 0;
-    const seq = sequence.reverse();
-
-    for (let i = 0; i < seq.length; i++) {
-        let pointX;
-        let pointY;
-        if (i === 0) {
-            pointX = 0;
-            pointY = 0;
-        } else {
-            // const vector = new THREE.Vector3(prevPoint[0], prevPoint[1] + d, 0)
-            // console.log(vector)
-            relativeAngle = relativeAngle + (seq[i] % 2 === 0 ? alpha : -alpha);
-            // const axis = new THREE.Vector3(0,0,-1); // rotate around z axis
-            // vector.applyAxisAngle(axis, relativeAngle);
-            // pointX = vector.x;
-
-            // pointY = vector.y;
-            pointX = prevPoint[0] + d * Math.sin(relativeAngle);
-            pointY = prevPoint[1] + d * Math.cos(relativeAngle);
-        }
-        points.push([pointX, pointY]);
-        prevPoint = [pointX, pointY];
-    }
-
-    const vectors = points.map((point) => {
-        return new THREE.Vector3(point[0], point[1], 0);
-    });
-    return vectors;
-}
-
-const getMeshObjects = () => {
-    const meshObjects = [];
-    const params = {
-        extrusionSegments: 100,
-        radiusSegments: 12,
-    };
-    const n = 100;
-    for (let i = 2; i <= n; i++) {
-        const seq = getCollatzSequence(i);
-        const vectors = createSplinePathFromCollatz(seq);
-        vectorsList.push(vectors);
-
-        // create a spline from all the vector points
-        const spline = new THREE.CatmullRomCurve3(vectors);
-        spline.curveType = "catmullrom";
-        spline.closed = false;
-
-        // create tube geometry based on spline
-        const geometry = new THREE.TubeGeometry(
-            spline,
-            params.extrusionSegments,
-            2,
-            params.radiusSegmentsl
-        );
-        const material = new THREE.MeshNormalMaterial();
-        material.side = THREE.DoubleSide;
-        const mesh = new THREE.Mesh(geometry, material);
-
-        meshObjects.push(mesh);
-    }
-    return meshObjects;
-};
-
-let meshes = getMeshObjects();
-function recalculateMeshes() {
-    meshes = getMeshObjects();
-}
-
-
-function App() {
+const { nodeDistance, bendAngle,sequenceLimit } = useControls({ nodeDistance: d, bendAngle: alpha, sequenceLimit : n })
 
     return (
         <div className="App">
-            {meshes.length > 0 && (
-                <CanvasContainer
-                    meshObjects={meshes}
-                    vectorsList={vectorsList}
-                    drawDebugLine={false}
-                ></CanvasContainer>
-            )}
+        <CanvasContainer
+        nodeDistance={nodeDistance}
+            bendAngle={bendAngle}
+            sequenceLimit={sequenceLimit}
+        ></CanvasContainer>
         </div>
     );
 }
--- src/CanvasContainer.js
+++ src/CanvasContainer.js
@@ -2,47 +2,85 @@ import { Canvas, useThree } from "@react-three/fiber";
 import React from "react";
 import * as THREE from "three";
 import { OrbitControls } from "@react-three/drei";
+import { getCollatzSequence } from "./helpers";
 
-const CanvasContainer = ({ meshObjects, vectorsList, drawDebugLine }) => {
-    const Foo = () => {
-        const group = new THREE.Group();
 
-        const {
-            scene,
-            camera,
-            gl: { domElement },
-        } = useThree();
-        meshObjects.forEach((mesh) => {
-            group.add(mesh);
-        });
-        if (drawDebugLine) {
-            vectorsList.forEach((vectors) => {
-                //create a blue LineBasicMaterial
-                const material = new THREE.LineBasicMaterial({
-                    color: 0x0000ff,
-                });
-                const geometry = new THREE.BufferGeometry().setFromPoints(
-                    vectors
-                );
-                const line = new THREE.Line(geometry, material);
-                group.add(line);
-            });
+const tubeGeometryParams = {
+    extrusionSegments: 100,
+    radiusSegments: 12,
+    radius: 1
+};
+
+function createSplinePathFromCollatz(sequence, nodeDistance, bendAngle) {
+    const points = [];
+    let prevPoint;
+    let relativeAngle = 0;
+    const seq = sequence.reverse();
+
+    for (let i = 0; i < seq.length; i++) {
+        let pointX;
+        let pointY;
+        if (i === 0) {
+            pointX = 0;
+            pointY = 0;
+        } else {
+            // const vector = new THREE.Vector3(prevPoint[0], prevPoint[1] + nodeDistance, 0)
+            // console.log(vector)
+            relativeAngle = relativeAngle + (seq[i] % 2 === 0 ? bendAngle : -bendAngle);
+            // const axis = new THREE.Vector3(0,0,-1); // rotate around z axis
+            // vector.applyAxisAngle(axis, relativeAngle);
+            // pointX = vector.x;
+
+            // pointY = vector.y;
+            pointX = prevPoint[0] + nodeDistance * Math.sin(relativeAngle);
+            pointY = prevPoint[1] + nodeDistance * Math.cos(relativeAngle);
         }
+        points.push([pointX, pointY]);
+        prevPoint = [pointX, pointY];
+    }
 
-        group.position.set(0, 0, 0);
-        group.scale.set(0.01, 0.01, 0.01);
+    const vectors = points.map((point) => {
+        return new THREE.Vector3(point[0], point[1], 0);
+    });
+    return vectors;
+}
 
-        // adds x,y,z axes
-        // const axesHelper = new THREE.AxesHelper(5);
-        // scene.add(axesHelper);
+const getNodeVectorsList = (n, nodeDistance, bendAngle) => {
+    const vectorsList = [];
+    for (let i = 2; i <= n; i++) {
+        const seq = getCollatzSequence(i);
+        const vectors = createSplinePathFromCollatz(seq, nodeDistance, bendAngle);
+        vectorsList.push(vectors);
+    }
+    return vectorsList;
+};
 
-        scene.add(group);
-        return <mesh></mesh>;
-    };
+const CanvasContainer = ({ nodeDistance, bendAngle, sequenceLimit }) => {
+
+    const vectorsList = getNodeVectorsList(sequenceLimit, nodeDistance, bendAngle);
+
     return (
         <Canvas>
             <OrbitControls enableDamping enablePan enableRotate enableZoom />
-            <Foo></Foo>
+            <group position={[0, 0, 0]} scale={[0.01, 0.01, 0.01]}>
+                {vectorsList.map((vectors, index) => {
+                    // create a spline from all the vector points
+                    const spline = new THREE.CatmullRomCurve3(vectors);
+                    spline.curveType = "catmullrom";
+                    spline.closed = false;
+                    const randRotX = Math.random()/2;
+                    const randRotY = Math.random()/2;
+                    const randRotZ = Math.random()/2;
+                    return <mesh key={index} rotation-x={randRotX} rotation-y={randRotY} rotation-z={randRotZ}>
+                        <tubeGeometry args={[spline,
+                            tubeGeometryParams.extrusionSegments,
+                            tubeGeometryParams.radius,
+                            tubeGeometryParams.radiusSegments]} />
+                        <meshNormalMaterial side={THREE.DoubleSide} />
+                    </mesh>
+
+                })}
+            </group>
         </Canvas>
     );
 };
